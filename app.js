b050abd49c132152dbf5c8acbee96bd0241851fb
Ramon-Goveia <ramonalangoveia@hotmail.com>
2020-07-02 15:05:43 -0300

    API-Node.js

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
+const path = require('path');
+
 const app = express();
 
 require('./models/home');
 const Home = mongoose.model('Home');
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE');
+  res.header("Access-Control-Allow-Headers", 'X-PINGOTHER, Content-Type, Authorization');
+  app.use(cors());
+  next();
+});
+
+app.use(
+  // '/file' para ocultar caminho
+  '/temp/uploads',
+  express.static(path.resolve(__dirname, 'temp','uploads'))
+);
 
 //Conexão BD
 mongoose.connect('mongodb://localhost/db-node', {
@@ -16,25 +35,121 @@ mongoose.connect('mongodb://localhost/db-node', {
 });
  
 app.get('/home', (req, res) => {
-  res.json({
-    error: false,
-    message: "Informações da Home-Page"
+  Home.findOne({}).then((home) => {
+    if(home.portUmFileName){
+      var portUmFileName = "http://localhost:8080/temp/uploads/home/" + home.portUmFileName;
+    }else{
+      var portUmFileName = "http://localhost:8080/temp/uploads/home/portolio.jpg";
+    }
+    if(home.portDoisFileName){
+      var portDoisFileName = "http://localhost:8080/temp/uploads/home/" + home.portDoisFileName;
+    }else{
+      var portDoisFileName = "http://localhost:8080/temp/uploads/home/portolio.jpg";
+    }
+    if(home.portTresFileName){
+      var portTresFileName = "http://localhost:8080/temp/uploads/home/" + home.portTresFileName;
+    }else{
+      var portTresFileName = "http://localhost:8080/temp/uploads/home/portolio.jpg";
+    }
+    if(home.portQuatroFileName){
+      var portQuatroFileName = "http://localhost:8080/temp/uploads/home/" + home.portQuatroFileName;
+    }else{
+      var portQuatroFileName = "http://localhost:8080/temp/uploads/home/portolio.jpg";
+    }
+    if(home.portCincoFileName){
+      var portCincoFileName = "http://localhost:8080/temp/uploads/home/" + home.portCincoFileName;
+    }else{
+      var portCincoFileName = "http://localhost:8080/temp/uploads/home/portolio.jpg";
+    }
+    if(home.portSeisFileName){
+      var portSeisFileName = "http://localhost:8080/temp/uploads/home/" + home.portSeisFileName;
+    }else{
+      var portSeisFileName = "http://localhost:8080/temp/uploads/home/portolio.jpg";
+    }
+    
+
+    return res.json({
+      home,
+      portUmFileName,
+      portDoisFileName,
+      portTresFileName,
+      portQuatroFileName,
+      portCincoFileName,
+      portSeisFileName
+    });
+  }).catch((err) => {
+    return res.status(400).json({
+      error: true,
+      message: "Nenhum registro encontrado!"
+    });
   });
 });
 
 app.post('/home', (req, res) => {
-  Home.create(req.body, (err) => {
+  const dados = {
+    "topTitle":"Temos a solução que sua empresa precisa!",
+	"topSubTitle":"This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.",
+	"topTextBtn":"ENTRE EM CONTATO",
+	"topLinkBtn":"http://localhost:3000/",
+	"servTitle":"Serviços",
+  "servSubTitle":"Featured content or information",
+  "servUmIcone":"code",
+  "servUmTitle":"Serviço 1",
+  "servUmDesc":"Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+  "servDoisIcone":"mobile-alt",
+  "servDoisTitle":"Serviço 2",
+  "servDoisDesc":"Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+  "servTresIcone":"bullhorn",
+  "servTresTitle":"Serviço 3",
+ 	"servDescDesc":"Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
+  "portTitle":"Portifólio",
+  "portSubTitle":"Featured content or information",
+  "portUmOriginalName":"portolio_um.jpg",
+  "portUmFileName":"portolio_um.jpg",
+  "portUmTitle":"Card title",
+  "portUmSubTitle":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  "portDoisOriginalName":"portolio_dois.jpg",
+  "portDoisFileName":"portolio_dois.jpg",
+  "portDoisTitle":"Card title",
+  "portDoisSubTitle":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  "portTresOriginalName":"portolio_tres.jpg",
+  "portTresFileName":"portolio_tres.jpg",
+  "portTresTitle":"Card title",
+  "portTresSubTitle":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  "portQuatroOriginalName":"portolio_quatro.jpg",
+  "portQuatroFileName":"portolio_quatro.jpg",
+  "portQuatroTitle":"Card title",
+  "portQuatroSubTitle":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  "portCincoOriginalName":"portolio_cinco.jpg",
+  "portCincoFileName":"portolio_cinco.jpg",
+  "portCincoTitle":"Card title",
+  "portCincoSubTitle":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+  "portSeisOriginalName":"portolio_seis.jpg",
+  "portSeisFileName":"portolio_seis.jpg",
+  "portSeisTitle":"Card title",
+  "portSeisSubTitle":"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+  };
+
+  const homeExist = Home.findOne({});
+  if(homeExist){
+    return res.status(400).json({
+      error: true,
+      message: "Erro: A paágina home já possui um registro!"
+    });
+  };
+
+  Home.create(dados, (err) => {
     if(err) return res.status(400).json({
       error: true,
       message: "Erro ao se conectar com BD"
-    })
-  })
+    });
+  });
 
   return res.json({
     error: true,
     message: "Conteúdo registrado com sucesso!"
-  })
-})
+  });
+});
  
 app.listen(8080, () => {
     console.log("Servidor iniciado na porta 8080: http://localhost:8080");
--- models/home.js
+++ models/home.js
@@ -13,11 +13,122 @@ var home = new Schema({
     },
     topLinkBtn:{
         type: String
-    }
-  },
+    },
+    servTitle:{
+        type: String
+    },
+    servSubTitle:{
+        type: String
+    },
+    servUmIcone:{
+        type: String
+    },
+    servUmTitle:{
+        type: String
+    },
+    servUmDesc:{
+        type: String
+    },
+    servDoisIcone:{
+        type: String
+    },
+    servDoisTitle:{
+        type: String
+    },
+    servDoisDesc:{
+        type: String
+    },
+    servTresIcone:{
+        type: String
+    },
+    servTresTitle:{
+        type: String
+    },
+    servDescDesc:{
+        type: String
+    },
+    portTitle:{
+        type: String
+    },
+    portSubTitle:{
+        type: String
+    },
+    portUmOriginalName:{
+        type: String
+    },
+    portUmFileName: {
+        type: String
+    },
+    portUmTitle:{
+        type: String
+    },
+    portUmSubTitle:{
+        type: String
+    },
+    portDoisOriginalName:{
+        type: String
+    },
+    portDoisFileName: {
+        type: String
+    },
+    portDoisTitle:{
+        type: String
+    },
+    portDoisSubTitle:{
+        type: String
+    },
+    portTresOriginalName:{
+        type: String
+    },
+    portTresFileName: {
+        type: String
+    },
+    portTresTitle:{
+        type: String
+    },
+    portTresSubTitle:{
+        type: String
+    },
+    portQuatroOriginalName:{
+        type: String
+    },
+    portQuatroFileName: {
+        type: String
+    },
+    portQuatroTitle:{
+        type: String
+    },
+    portQuatroSubTitle:{
+        type: String
+    },
+    portCincoOriginalName:{
+        type: String
+    },
+    portCincoFileName: {
+        type: String
+    },
+    portCincoTitle:{
+        type: String
+    },
+    portCincoSubTitle:{
+        type: String
+    },
+    portSeisOriginalName:{
+        type: String
+    },
+    portSeisFileName: {
+        type: String
+    },
+    portSeisTitle:{
+        type: String
+    },
+    portSeisSubTitle:{
+        type: String
+    },
+},
     {
         timestamps: true,
     }
-  );
+);
 
-  mongoose.model('Home', home);
+mongoose.model('Home', home);
